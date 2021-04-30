5e61b58fa43af91c6ebe72a1584ccda896096524
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-29 22:48:33 -0300

    Criando geração de site estatico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
-import { Header } from "../components/Header";
+// SPA => Tradicional   useEffect(() => {}, []); // Carregados momentos que carrega a tela // não e index pelo google.
+// SSR => Dados carregada pela camada de servidor // inde pelo google  //Execita toda vez que usuario entrar na home
+// SSG => Versão estatica // não faz requisição nova
 
-export default function Home() {
-  return <h1>Index</h1>;
+export default function Home(props) {
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/episodes")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
+  console.log(props.episodes);
+
+  return (
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
 }
+
+//Html estatico
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // Gerar chama api cada 8h
+  };
+}
+
+//yarn build
+//yarn start => tudo funcional do next
