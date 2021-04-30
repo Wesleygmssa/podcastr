3fd2f63a2625b93c4b9966da34f05abd39dd08da
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-29 23:24:47 -0300

    Configurando axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,30 @@
 // SSR => Dados carregada pela camada de servidor // inde pelo google  //Execita toda vez que usuario entrar na home
 // SSG => Versão estatica // não faz requisição nova
 import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+};
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   // useEffect(() => {
   //   fetch("http://localhost:3333/episodes")
   //     .then((response) => response.json())
   //     .then((data) => console.log(data));
   // }, []);
-
-  console.log(props.episodes);
 
   return (
     <>
@@ -22,8 +37,14 @@ export default function Home(props) {
 
 //Html estatico
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+  //_limit=12&_sort=published_at&_order=desc
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
 
   return {
     props: {
