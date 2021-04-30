6c187fb03708ac8b8fd4d516123fe9659de2b5cf
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-29 23:41:11 -0300

    Formatando os dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 // SPA => Tradicional   useEffect(() => {}, []); // Carregados momentos que carrega a tela // não e index pelo google.
 // SSR => Dados carregada pela camada de servidor // inde pelo google  //Execita toda vez que usuario entrar na home
 // SSG => Versão estatica // não faz requisição nova
+import { parseISO, format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { GetStaticProps } from "next";
 import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString.ts";
 
 type Episode = {
   id: string;
@@ -39,12 +42,34 @@ export default function Home(props: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   //_limit=12&_sort=published_at&_order=desc
   const { data } = await api.get("episodes", {
+    //chama api
     params: {
       _limit: 12,
       _sort: "published_at",
       _order: "desc",
     },
   });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2);
 
   return {
     props: {
