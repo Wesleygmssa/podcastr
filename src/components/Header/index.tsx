7404b976e5307b9bfc62b0f28caea453de3133cd
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-29 21:11:08 -0300

    Criando compoente de Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
 export function Header() {
+  //Fezendo a transformação da data
   const currentDate = format(new Date(), "EEEEEE, d MMMM", { locale: ptBR });
   return (
     <header className={styles.headerContainer}>
