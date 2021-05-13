5e65819ea9eda82955063c4cafd54c027185b91b
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-12 22:03:52 -0300

    Ajustando estilização dos buttons

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -51,19 +51,27 @@ export function Player() {
           <span>00:00</span>
         </div>
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.ṕlayButton}>
+          <button
+            type="button"
+            className={styles.ṕlayButton}
+            disabled={!episode}
+          >
             <img src="/play.svg" alt="Tocar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
-          <button type="button" className={styles.ṕlayButton}>
+          <button
+            type="button"
+            className={styles.ṕlayButton}
+            disabled={!episode}
+          >
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
