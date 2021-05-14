6098fa764ba93cbd4340a45ca6948855d3bbef5f
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-13 22:03:59 -0300

    Ajuste do contexto

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 type Episode = {
   title: string;
@@ -17,10 +17,53 @@ type PlayerContextData = {
   togglePlay: () => void;
 };
 
+type PlayContextProviderProps = {
+  children: ReactNode;
+};
+
 /* ***Poderia ser dessa forma
 export const PlayerContext = createContext({
   episodeList: [],
   currentEpisodeIndex: 0,
 }); */
 
+//variavel para armarzenar dados do contexto
 export const PlayerContext = createContext({} as PlayerContextData);
+
+//Contexto que estava no APP;
+export function PlayerContextProvider({ children }: PlayContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying); //true || false
+  }
+  function setPlayState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    /* ***
+     *Aqui esta variavel global da aplicação
+     **** */
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayState,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
